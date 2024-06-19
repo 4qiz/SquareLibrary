5ecbed0ec0153da065d16493bbb709976612a1c2
EgorPC <egor.antipin.1995mail.ru@gmail.com>
2024-06-19 20:16:42 +0300

    Добавлены xml комментарии

--- SquareLibrary/GeometryCalculator.cs
+++ SquareLibrary/GeometryCalculator.cs
@@ -7,8 +7,16 @@ using System.Threading.Tasks;
 
 namespace SquareLibrary
 {
+    /// <summary>
+    /// Класс для вычисления площади геометрических фигур
+    /// </summary>
     public class GeometryCalculator
     {
+        /// <summary>
+        /// Вычисляет площадь переданной фигуры
+        /// </summary>
+        /// <param name="shape">Фигура, для которой нужно вычислить площадь</param>
+        /// <returns>Значение площади указанной фигуры</returns>
         public double CalculateArea(IShape shape)
         {
             return shape.CalculateArea();
